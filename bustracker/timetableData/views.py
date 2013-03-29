3f9f093ea49932eee8c275b702e65bc4730c67f5
ChrsT <me@christophertaylor.net>
2013-03-29 19:58:58 +0000

    Single journey query

--- bustracker/timetableData/views.py
+++ bustracker/timetableData/views.py
@@ -16,9 +16,16 @@ def one_journey(request):
 			return render(request,"bus_stop_to.html", {"bus_stop_form" : stops_form, "stops" : timetableData.query.get_from([source], destination, time, date.weekday())})
 	stops_form = PickStops()
 	return render(request,"bus_stop_to.html", {"bus_stop_form" : stops_form})
-class PickStops(forms.Form):
-	source = forms.ModelChoiceField(queryset=BusStop.objects.all())
-	destination = forms.ModelChoiceField(queryset=BusStop.objects.all())
 
-	date = forms.DateField(initial=datetime.date.today())
-	time = forms.TimeField(initial=datetime.time(7,0,0))
+class BusStopModelFormChoice(forms.ModelChoiceField):
+	def label_from_instance(self,obj):
+		if not isinstance(obj, BusStop):
+			raise
+		return obj.name
+
+class PickStops(forms.Form):
+	source = BusStopModelFormChoice(queryset=BusStop.objects.all(), label="From: ")
+	destination = BusStopModelFormChoice(queryset=BusStop.objects.all(), label="To: ")
+
+	date = forms.DateField(initial=datetime.date.today(), label="Date: ")
+	time = forms.TimeField(initial=datetime.time(7,0,0), label="Arrival by: ")
